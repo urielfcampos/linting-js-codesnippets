a2ce6474e8477f5126f4bfb71abd1912064fe3ca
urielfcampos <urielfcampos95@gmail.com>
2019-01-29 00:04:13 -0300

    Edited the python script to output a more useful CSV file after tests

--- scripts/parser.py
+++ scripts/parser.py
@@ -5,7 +5,7 @@ import glob
 import os
 
 class Parser:
-
+    postId =''
     def __init__(self):
         self.jdic = {}
 
@@ -16,17 +16,54 @@ class Parser:
     def parse_errors(self, l):
         for dic in l:
             for k, v in dic.items():
-                #print(k,v)
+                if k == "filePath":
+                    self.postId = self.getPostId(v)
                 if k == 'ruleId':
-                    self.jdic[v] = 1 if v not in self.jdic else self.jdic[v] + 1
+                    self.jdic[self.postId][v] = 1 if v not in self.jdic[self.postId] else self.jdic[self.postId][v] + 1
                 if isinstance(v, list):
                     self.parse_errors(v)
 
-    def out_to_csv(self, dic, filename,openType):
+    def initializeDict(self, dict):
+        for subDict in dict:
+            for k, v in subDict.items():
+                if k == "filePath":
+                    v = self.getPostId(v)
+                    self.jdic[v] = {}
+
+    def getPostId(self, filepath:str):
+        filepath = filepath.split("/").pop().split(".").pop(0)
+        return filepath
+
+    def out_to_csv(self, dic, filename,openType,headers = []):
+        self.ExtractRules(dic, headers)
         with open(filename, openType, newline='') as file:
-            writer = csv.DictWriter(file, dic.keys())
+            writer = csv.DictWriter(file, fieldnames=headers)
             writer.writeheader()
-            writer.writerow(dic)
+            for x in dic:
+                writer.writerow(self.createCsvDict(x,dic[x],headers))
+
+
+    def ExtractRules(self, dic, headers):
+        for x in dic:
+            for y in dic[x]:
+                if y not in headers:
+                    headers.append(y)
+
+    def createCsvDict(self,key,dic,headers):
+        csvDict = {}
+        for x in headers:
+            csvDict[x] = ''
+        csvDict['Post Id'] = key
+        for keys,values in dic.items():
+            csvDict[keys] = values
+        return csvDict
+
+
+
+
+
+
+
 
 parser = Parser()
 l = []
@@ -38,9 +75,11 @@ for x in fileDir:
     l.append(parser.parse_json(x))
 for x in l:
     #print(x)
+    parser.initializeDict(x)
     parser.parse_errors(x)
-for x in l:
-    for y in x:
-        parser.out_to_csv(y,"IndividualReport.csv",'a')
+    print(parser.jdic)
+#for x in l:
+#    for y in x:
+#        parser.out_to_csv(y,"IndividualReport.csv",'a')
 ##print("PARSED: ", parser.jdic)
-parser.out_to_csv(parser.jdic, 'output.csv','w')
+parser.out_to_csv(parser.jdic, 'PersonalizedIndividualReport.csv','w',["Post Id"])
